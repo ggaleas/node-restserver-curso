2a80466f23ccbf520796bca462f27491dbfb8054
ggaleas <ggaleas@gmail.com>
2020-11-01 12:43:32 -0300

    RestServer - Usuario

--- server/config/config.js
+++ server/config/config.js
@@ -2,3 +2,22 @@
 //    Puerto
 //========================
 process.env.PORT = process.env.PORT || 3000;
+
+
+//========================
+//    Entorno
+//========================
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+//========================
+//    Base de Datos
+//========================
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb+srv://user-admin:6NF9vPbxLXPJkt6W@cluster0.35svx.mongodb.net/cafe';
+}
+
+process.env.URLDB = urlDB
