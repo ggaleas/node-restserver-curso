e34b965524bcf86136eee10755c9107f0cd8cab8
ggaleas <ggaleas@gmail.com>
2020-11-01 13:33:19 -0300

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -17,7 +17,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://user-admin:6NF9vPbxLXPJkt6W@cluster0.35svx.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB
